a628095f9e330caf0a2ec407ade811a40d77435a
Shikhar Srivastava <shikharsrivastava234@gmail.com>
2025-02-28 18:09:57 +0000

    spring project initialized

---
+++ pictionary/src/main/java/com/shikhar/pictionary/PictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.shikhar.pictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictionaryApplication.class, args);
+	}
+
+}
---
+++ pictionary/src/test/java/com/shikhar/pictionary/PictionaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shikhar.pictionary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PictionaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
